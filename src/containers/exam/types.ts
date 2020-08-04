366cd9796ce0509758d487c2d134b22fb96a8e88
toxdes <malivp3494@gmail.com>
2020-08-04 19:07:32 +0530

    Fake model for the exam data
    which will be replaced by the API later,
    also added uuid lib to generate a unique ID

--- src/containers/exam/types.ts
+++ src/containers/exam/types.ts
@@ -22,3 +22,29 @@ export interface StepProps {
   onNextStep: () => void;
   onPreviousStep?: () => void;
 }
+
+type TCandidateData = {
+  name: string;
+  avatar: string;
+  rollNumber: string;
+};
+
+export type TQuestionType = "mcq" | "numeric";
+
+export type TQuestion = {
+  id: string;
+  text: string;
+  type: TQuestionType;
+  choices?: string[];
+};
+
+export type TExamData = {
+  streamName: string;
+  examName: string;
+  subjects: string[];
+  sections: string[];
+  candidateData: TCandidateData;
+  questions: TQuestion[][];
+  totalTimeInMinutes: number;
+  calculatorAllowed: boolean;
+};
