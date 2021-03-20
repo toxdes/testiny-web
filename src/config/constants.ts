904cf4b3d92e7bb349921bca569d8907d118423f
toxicdesire <malivp3494@gmail.com>
2021-03-21 00:13:41 +0530

    Connecting to heroku-backend #8

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 const DEBUG_MODE = false;
-let API_BASE_URL = "http://testiny-backend.herokuapp.com/";
+let API_BASE_URL = "https://testiny-backend.herokuapp.com/";
 
 if (process.env.ENV === "production") {
   API_BASE_URL = process.env.BACKEND_URL as string;
