388f0e27434a5acb5098c3ba5b87e08bdf718c27
Vaibhav Mali <malivp3494@gmail.com>
2022-08-05 16:12:03 +0530

    Update backend-url
    Free quota of EC2 exceeded so made another account.

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,8 +4,8 @@ const DEBUG_MODE = false;
 // link to source code
 const PROJECT_LINK = "https://github.com/toxdes/testiny-web";
 
-let API_BASE_URL = "https://api.testiny.live/";
-// let API_BASE_URL = "http://172.25.241.170:8000";
+// let API_BASE_URL = "https://api.testiny.live/";
+let API_BASE_URL = "http://172.31.12.44:8000";
 // let API_BASE_URL = "http://localhost:8000/";
 
 // TODO: Figure out a flawless way to handle production builds
